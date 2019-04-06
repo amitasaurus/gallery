eb753b730e8f63ea4c8f396dc02d33d8a9d1e21a
Amit Singh <amit@coinswitch.co>
2019-04-06 14:35:51 +0530

    Setting Up Automation Task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,30 @@
-/** Serving Library Files directly from CDN and script tag **/
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const csso = require('gulp-csso');
+
+// Default Task
+gulp.task('default', ['sass']);
+
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.css'))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(csso())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('build/css'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
