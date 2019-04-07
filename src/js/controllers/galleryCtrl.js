c84c786ae0f92ebef506432c23e9a5dadcf698ec
Amit Singh <amit@coinswitch.co>
2019-04-07 19:25:36 +0530

    [UPDATE]: Filter UI

--- src/js/controllers/galleryCtrl.js
+++ src/js/controllers/galleryCtrl.js
@@ -1,4 +1,18 @@
-app.controller('galleryCtrl', function($scope){
-	console.log('galleryCtrl');
-	$scope.images = new Array(24);
+app.controller('galleryCtrl', function($scope) {
+    console.log('galleryCtrl');
+    $scope.images = new Array(24);
+
+    $scope.filterCtrl = new function() {
+        this.isActive = false;
+        this.display = () => {
+            this.isActive = true;
+        }
+        this.hide = (hide) => {
+            this.isActive = false;
+        }
+        this.update = (tag) => {
+        	console.log(tag);
+        	this.hide();
+        }
+    }
 })
